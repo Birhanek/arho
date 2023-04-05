4c8f0ce5e46bcb4659abd443be0fc1c3c5ca4d63
muez-haftu <muezhaftu721@gmail.com>
2023-04-05 21:35:00 +0200

    navbar and footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Footer = () => {
+
+
+  return (
+    <div className="App">
+      Welcome to Aroho official product site
+    </div>
+  );
+}
+
+export default Footer;
