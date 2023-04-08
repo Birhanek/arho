ccde3ad0ec9450b1ff960fd42bf08c30977be488
Birhane Kahsay <birhane.kahsay99@gmail.com>
2023-04-08 17:49:12 +0200

    adding search layout

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -5,18 +5,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { Images } from '../../DataInterface/productInterface'
 import { Link } from 'react-router-dom'
+import Search from '../search/Search';
 const Navbar = () => {
   return (
-    <Box sx={{flexGrow:1}}>
-      <AppBar position='sticky'>
+    <Box sx={{flexGrow:1}} >
+      <AppBar position='sticky' elevation={0}>
         <Toolbar>
-          <IconButton size='large' edge='start' aria-label='open-drawer' sx={{display:{
-            xs:'block',sm:'none',md:'none',lg:'none'
-          }}}>
+          <IconButton color='primary' sx={{display:{xs:'block',sm:'none',md:'none',lg:'none'},backgroundColor:'red'}}>
             <MenuIcon/>
           </IconButton>
           <Link to='/'><img style={{width:100,height:100,margin:4}} src={Images.logo.src} alt={Images.logo.alt}/></Link>
-          
+          <Search/>
         </Toolbar>
       </AppBar>
       <PlayArrowIcon sx={{backgroundColor:'yellow'}}/>
