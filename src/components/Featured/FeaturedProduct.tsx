8d5916c931108aa617c628f0f304cda6f389fc6b
Birhane Kahsay <birhane.kahsay99@gmail.com>
2023-04-13 17:41:35 +0200

    adding pagination and some features in the product

--- src/components/Featured/FeaturedProduct.tsx
+++ src/components/Featured/FeaturedProduct.tsx
@@ -1,27 +1,48 @@
 import { Container, Paper, Typography } from '@mui/material'
-import React, { useEffect } from 'react'
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight'
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft'
+import React, { useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hook'
 import { getAllProduct } from '../../DataInterface/productApi'
 import Loading from '../SideFeatures/Loading'
 import Product from './Product'
+import { grey } from '@mui/material/colors'
 
 const FeaturedProduct = () => {
     const  { products, isLoading, isError , message } = useAppSelector(state => state.productR)
 
+    const [manipulator,setManipulator] = useState<number>(0)
+    const [dataPerPage] = useState<number>(10)
+
+    const indexOfLastData = manipulator * dataPerPage + dataPerPage
+    const indexOfFirstData = manipulator * dataPerPage
     const dispatch = useAppDispatch()
      
     console.log(products)
-    const productElement =  products.products.map((product, index) =>{
+    const productElement =  products.products.slice(indexOfFirstData,indexOfLastData).map((product, index) =>{
 
         return <Product index={index} product={product}/>
     })
+
+    const totalPage = products.limit / dataPerPage;
+
+    const prevPage = () =>{
+        if(manipulator !== 0){
+            setManipulator(manipulator-1)
+        }
+    }
+    const nextPage = () => {
+        if(manipulator !== totalPage-1){
+            setManipulator(manipulator + 1)
+        }
+    }
 
     useEffect(()=>{
         dispatch( getAllProduct() )
     },[dispatch])
 
   return (
-    <Paper >
+    <Paper className='feature-container'>
         <Typography variant='h1' component='h1' textAlign='center' padding='2rem'>
             Featured Products
         </Typography>
@@ -30,13 +51,18 @@ const FeaturedProduct = () => {
         <Loading/> : 
         isError ? 
         <Typography variant='body1' >{message?.toLocaleUpperCase()}</Typography>:
-        <Container className='feature-container' sx={{display:'grid',gridTemplateColumns:'repeat(4,minMax(0,1fr))',gap:2}}>
+        <Container  sx={{display:'grid',gridTemplateColumns:'repeat(4,minMax(0,1fr))',gap:2}}>
             {
                products && productElement
             }
+            
         </Container>
         
         }
+        <div className='pagination'>
+            <ArrowCircleLeftIcon   onClick = {prevPage}/>
+            <ArrowCircleRightIcon  onClick = {nextPage}/>
+        </div>
     </Paper>
   )
 }
